31e6afef626c28105d51737bc7fcb411009e2e82
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-12-08 14:14:42 +0530

    made the urls and views dynamic

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -4,8 +4,4 @@ from django.urls import path
 # importing the view file from the same folder as the current urls file exist
 from . import views
 
-urlpatterns = [
-    # it means that if a request reaches /january then execute this index view function, this creates a url config
-    path("january", views.january),
-    path("february", views.february),
-]
+urlpatterns = [path("<month>", views.monthly_challenge)]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Eat no mean for the entire month of January!")
-
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes every day in February!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no mean for the entire month of January!"
+    elif month == "february":
+        challenge_text = "Walk for at least 20 minutes every day in February!"
+    elif month == "march":
+        challenge_text = (
+            "Learn Django for at least 20 minutes every day for the month of March!"
+        )
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
