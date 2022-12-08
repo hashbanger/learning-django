fc61ffe8c6c7d791b0347f5d8921828e70b98ae8
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-12-08 13:51:27 +0530

    added the first view pattern

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,10 @@
+# This file contains the urls for the app
+from django.urls import path
+
+# importing the view file from the same folder as the current urls file exist
+from . import views
+
+urlpatterns = [
+    # it means that if a request reaches /january then execute this index view function, this creates a url config
+    path("january", views.index)
+]
--- monthly_challenges/monthly_challenges/urls.py
+++ monthly_challenges/monthly_challenges/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# This contains the urls for the entire project
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # it means add all the urls for the challenges application
+    path("challenges/", include("challenges.urls")),
 ]
