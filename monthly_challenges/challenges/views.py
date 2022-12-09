22d72d2281c0b4f0a250ac5a4249b18fa265f58a
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-12-09 12:28:18 +0530

    made the view dynamic

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse 
 
 monthly_challenges = {
     "january": "Eat no mean for the entire month of January!",
@@ -26,10 +27,11 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid Month!")
 
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # builds /challenges/month
+    return HttpResponseRedirect(redirect_path)
 
 
-def monthly_challenge(request, month):
+def  (request, month):
     challenge_text = None
     try:
         challenge_text = monthly_challenges[month]
