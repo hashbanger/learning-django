672ba259f39f60c32c63331bf500bdcb558be575
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-12-13 11:52:58 +0530

    404 template using django function

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat  for the entire month of January!",
@@ -53,5 +52,4 @@ def monthly_challenge(request, month):
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
     except:
-        response_data = render_to_string("404.html")
-        return HttpResponseNotFound(response_data)
+        raise Http404()
