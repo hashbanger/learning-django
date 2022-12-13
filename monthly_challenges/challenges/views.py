2e2a5d9d2168c5d9dbb2dbac60b965f117b44f97
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-12-13 11:46:48 +0530

    404 using render_to_string

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat  for the entire month of January!",
@@ -41,7 +42,6 @@ def monthly_challenge_by_number(request, month):
     return HttpResponseRedirect(redirect_path)
 
 
-
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
@@ -53,4 +53,5 @@ def monthly_challenge(request, month):
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
