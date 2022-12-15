3a108fedd9c1b3d4de01ddd72d2318848ced1512
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-12-15 16:06:55 +0530

    Login and Logout system

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -11,8 +13,15 @@ def register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}!")
-            return redirect("blog-home")
+            messages.success(
+                request, f"{{ username }} Your account has been, now you can log in!"
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+
+@login_required  # makes sure we don't access the profile page manually after logout
+def profile_view(request):
+    return render(request, "users/profile.html")
