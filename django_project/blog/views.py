54cb40200c1d9d31e00c4ed8e5434047eb30823d
hashbanger <prashant.brahmbhatt32@outlook.com>
2023-12-01 16:45:37 +0530

    worked on templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# adding dummy data for templates
+posts = [
+    {
+        "author": "PrashantB",
+        "title": "Blog Post",
+        "content": "first post content",
+        "date_posted": "December 1, 2023",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second blog post",
+        "date_posted": "December 1, 2023",
+    },
+]
 
 # Create your views here.
 
+
 # this function is going to take the traffic from the home page
 def home(request):
-    return HttpResponse("<h1>Welcome to the blog!</h1>")
+    context = {"posts": posts, "title": "Home"}
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
