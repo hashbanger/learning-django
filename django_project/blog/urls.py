d32009d933685c9d18415cfcdba35f8f93066d8d
hashbanger <prashant.brahmbhatt32@outlook.com>
2023-12-04 18:04:26 +0530

    reformatted code

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView)
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 from . import views
 
 urlpatterns = [
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
-    ListView, DetailView, CreateView, UpdateView, DeleteView)
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
 
 # Create your views here.
@@ -12,14 +17,19 @@ def home(request):
     context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
+
 class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
     context_object_name = "posts"
-    ordering = ["-date_posted"] # this will change the ordering of the content as per the date
+    ordering = [
+        "-date_posted"
+    ]  # this will change the ordering of the content as per the date
+
 
 class PostDetailView(DetailView):
     model = Post
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -28,6 +38,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
@@ -43,16 +54,18 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
 
     success_url = "/"
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         return False
 
+
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
-
